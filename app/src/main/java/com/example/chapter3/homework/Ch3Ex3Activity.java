7ce997d44a66b389ab3509f16deca03549fc08af
songyi <1641208406@qq.com>
2019-07-02 22:34:24 +0800

    init commit

---
+++ app/src/main/java/com/example/chapter3/homework/Ch3Ex3Activity.java
@@ -0,0 +1,51 @@
+package com.example.chapter3.homework;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 使用 ViewPager 和 Fragment 做一个简单版的好友列表界面
+ * 1. 使用 ViewPager 和 Fragment 做个可滑动界面
+ * 2. 使用 TabLayout 添加 Tab 支持
+ * 3. 对于好友列表 Fragment，使用 Lottie 实现 Loading 效果，在 5s 后展示实际的列表，要求这里的动效是淡入淡出
+ */
+public class Ch3Ex3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch3ex3);
+
+        // TODO: ex3-1. 添加 ViewPager 和 Fragment 做可滑动界面
+        ViewPager pager = findViewById(R.id.view_pager);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int i) {
+                return new Fragment();
+            }
+            @Override
+            public int getCount() {
+                return 2;
+            }
+
+            @Override
+            public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+                return false;
+            }
+        });
+
+        // TODO: ex3-2, 添加 TabLayout 支持 Tab
+    }
+}
